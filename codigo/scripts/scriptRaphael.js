8c91a66b660a2a88ab37d4f5c38c08a990d0ac2a
Raphael Diniz <rafagusto12@gmail.com>
2024-05-09 09:12:46 -0300

    edição pag de anotação

---
+++ codigo/scripts/scriptRaphael.js
@@ -0,0 +1,8 @@
+                var retan1 = document.getElementById('retan')
+                   var add1 = document.getElementById('add90')
+                   var main = document.getElementById('ok');
+                   
+                    add1.addEventListener("click", () => {
+                    var divClone = retan1.cloneNode(true);
+                    main.appendChild(divClone);
+                  })
