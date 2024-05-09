572792ef7fbee4aa4034d0cab009952b229f9615
Victor Alves <victor.alvessalcantara@gmail.com>
2024-05-09 20:30:35 -0300

    Atualizando a navbar

--- codigo/scripts/scriptRaphael.js
+++ codigo/scripts/scriptRaphael.js
@@ -1,8 +1,8 @@
-                var retan1 = document.getElementById('retan')
-                   var add1 = document.getElementById('add90')
-                   var main = document.getElementById('ok');
-                   
-                    add1.addEventListener("click", () => {
-                    var divClone = retan1.cloneNode(true);
-                    main.appendChild(divClone);
-                  })
+var retan1 = document.getElementById("retan");
+var add1 = document.getElementById("add90");
+var main = document.getElementById("ok");
+
+add1.addEventListener("click", () => {
+  var divClone = retan1.cloneNode(true);
+  main.appendChild(divClone);
+});
