d63045e22d19abca928358d44cd87c9797483c03
Raphael Diniz <rafagusto12@gmail.com>
2024-05-10 15:59:02 -0300

    removi o js antigo

--- codigo/scripts/scriptRaphael.js
+++ codigo/scripts/scriptRaphael.js
@@ -5,8 +5,6 @@
         var lastCloned = null;
 
 
-
-
         btn.addEventListener("click", () => {
             var divClone;
     if (lastCloned === null || lastCloned.classList.contains("retan3")) {
@@ -18,55 +16,7 @@
     }
     container.appendChild(divClone);
     lastCloned = divClone;
-
-    
         });
 
-        
-
-        var retangulos = [
-            { "titulo": "Título 1", "conteudo": "Conteúdo 1" },
-            { "titulo": "Título 2", "conteudo": "Conteúdo 2" },
-            { "titulo": "Título 3", "conteudo": "Conteúdo 3" }
-        ];
-
-        var botoesLapis = document.querySelectorAll(".lapis");
-botoesLapis.forEach(function(botao, index) {
-    botao.addEventListener("click", function() {
-        // Esconder botão de lápis e exibir botão de salvar
-        botao.style.display = "none";
-        var salvarBtn = botao.nextElementSibling;
-        salvarBtn.style.display = "inline-block";
-
-        // Tornar o título e o conteúdo editáveis
-        var retangulo = botao.parentNode;
-        var titulo = retangulo.querySelector(".retan-tit");
-        var conteudo = retangulo.querySelector(".retan-subtit");
-        titulo.contentEditable = true;
-        conteudo.contentEditable = true;
-    });
-});
-
-// Adicionar evento de clique aos botões de salvar de cada retângulo
-var botoesSalvar = document.querySelectorAll(".salvar");
-botoesSalvar.forEach(function(botao, index) {
-    botao.addEventListener("click", function() {
-        // Esconder botão de salvar e exibir botão de lápis
-        botao.style.display = "none";
-        var lapisBtn = botao.previousElementSibling;
-        lapisBtn.style.display = "inline-block";
-
-        // Tornar o título e o conteúdo não editáveis
-        var retangulo = botao.parentNode;
-        var titulo = retangulo.querySelector(".retan-tit");
-        var conteudo = retangulo.querySelector(".retan-subtit");
-        titulo.contentEditable = false;
-        conteudo.contentEditable = false;
-
-        // Atualizar os dados do retângulo
-        retangulos[index].titulo = titulo.textContent;
-        retangulos[index].conteudo = conteudo.textContent;
-    });
-});
 
     
