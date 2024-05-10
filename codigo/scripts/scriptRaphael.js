f455a17fa6aa2896d92e2f8bcee41acf6653f87a
Raphael Diniz <rafagusto12@gmail.com>
2024-05-10 14:57:31 -0300

    add retan alternados

--- codigo/scripts/scriptRaphael.js
+++ codigo/scripts/scriptRaphael.js
@@ -2,8 +2,20 @@
         var btn = document.querySelector(".add");
         var retan1 = document.querySelector(".retan1");
         var container = document.querySelector(".container");
+        var lastCloned = null;
 
+
+
+        
         btn.addEventListener("click", () => {
-            var divClone = retan1.cloneNode(true);
-            container.appendChild(divClone);
+            var divClone;
+    if (lastCloned === null || lastCloned.classList.contains("retan3")) {
+        divClone = document.querySelector(".retan1").cloneNode(true);
+    } else if (lastCloned.classList.contains("retan1")) {
+        divClone = document.querySelector(".retan2").cloneNode(true);
+    } else if (lastCloned.classList.contains("retan2")) {
+        divClone = document.querySelector(".retan3").cloneNode(true);
+    }
+    container.appendChild(divClone);
+    lastCloned = divClone;
         });
