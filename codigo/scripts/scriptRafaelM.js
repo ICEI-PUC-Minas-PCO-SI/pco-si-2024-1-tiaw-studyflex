fec55fe4bbcaa1758b867472e03b34aad8f7e66f
Rafael Melchiades <rafaelmelchiades1@gmail.com>
2024-05-12 20:36:35 -0300

    Progresso da Disciplina e Criação de Novas Tarefas

--- codigo/scripts/scriptRafaelM.js
+++ codigo/scripts/scriptRafaelM.js
@@ -1,10 +1,25 @@
+// Função para mostrar o progresso da tarefa da disciplina
 var nomeTarefa = document.querySelector("#nomeTarefa");
-    nomeTarefa.innerHTML = 'Matemática';
+nomeTarefa.innerHTML = 'Tarefa';
 
 const progresso = document.querySelector("#level-progress");
 const contador = document.querySelector("#nivelProgresso");
 
+progresso.style.width = ('45') + '%';
+contador.innerHTML = (progresso.style.width);
 
-    progresso.style.width = ('100') + '%';
-    contador.innerHTML = (progresso.style.width);
+
+
+//Função para criar tarefas
+function criarTarefa() {
+    var tarefaOriginal = document.getElementById('tasks-box');
+    var novaTarefa = tarefaOriginal.cloneNode(true);
+    novaTarefa.classList.add('tarefa');
+
+    var local= document.getElementById('divisao');
+    local.appendChild(novaTarefa);
+}
+
+
+  
 
