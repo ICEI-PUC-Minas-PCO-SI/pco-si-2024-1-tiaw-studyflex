b37495f255755ccce2d10fb5a75e0ad9a7a9c69b
Rafael Melchiades <rafaelmelchiades1@gmail.com>
2024-05-12 00:53:25 -0300

    Atualização do js do progresso de disciplina

--- codigo/scripts/scriptRafaelM.js
+++ codigo/scripts/scriptRafaelM.js
@@ -1,7 +1,15 @@
 
-    const progresso = document.querySelector("#level-progress");
-     progresso.style.width = '65' + '%';
-     progresso.innerHTML = '' + '%';
+const progresso = document.querySelector("#level-progress");
+const contador = document.querySelector('#progress-percentage')
 
-     
+    
+progresso.style.width = '65' + '%';
+progresso.innerHTML = '' + '%';
+
+
+function taskProgess() {
+    var porcentagem = document.querySelector('#level-progress').value;
+    document.querySelector('.progressoTarefa').innerHTML = porcentagem;
+
+}
 
