a6996da900c8b185b24af922a9f77770035411ba
Rafael Melchiades <rafaelmelchiades1@gmail.com>
2024-05-30 21:15:01 -0300

    Progresso das disciplinas

--- codigo/scripts/scriptRafaelM.js
+++ codigo/scripts/scriptRafaelM.js
@@ -1,22 +1,53 @@
 // Função para mostrar o progresso da tarefa da disciplina
-var nomeTarefa = document.querySelector("#nomeTarefa");
-nomeTarefa.innerHTML = 'Tarefa';
+//JSON Exemplo
+const dadosTarefas = {
+    "totalTarefas": 279,
+    "tarefasFeitas": 14
+};
 
-const progresso = document.querySelector("#level-progress");
-const contador = document.querySelector("#nivelProgresso");
+// Função para calcular a porcentagem de tarefas concluídas e atualizar o progresso
+function calcularPorcentagem(total, feitas) {
 
-progresso.style.width = ('79') + '%';
-contador.innerHTML = (progresso.style.width);
+    //Calcular a porcentagem
+    const porcentagem = (feitas / total) * 100;
+
+    //Limite de 0 a 100
+    const porcentagemLimite = Math.max(0, Math.min(100, porcentagem));
+
+    const progresso = document.querySelector("#level-progress");
+    const contador = document.querySelector("#nivelProgresso");
+    
+    //Verificador se o valor é inteiro
+    let porcentagemContador;
+    if (Number.isInteger(porcentagemLimite)){
+        porcentagemContador = porcentagemLimite.toString();
+    }
+    else{
+        porcentagemContador = porcentagemLimite.toFixed(1);
+        
+        //Verificador caso a casa decimal comece com 0 (Não mostrar caso ocorra)
+        if (porcentagemContador.endsWith('.0')){
+            porcentagemContador = porcentagemLimite.toFixed(0);
+        }
+    }
+
+    progresso.style.width = porcentagemLimite + '%';
+    contador.innerHTML = `${porcentagemContador}%`;
+}
+
+// Obter os valores do JSON e calcular a porcentagem de progresso
+const totalTarefas = dadosTarefas.totalTarefas;
+const tarefasFeitas = dadosTarefas.tarefasFeitas;
+calcularPorcentagem(totalTarefas, tarefasFeitas);
 
 //Função para criar tarefas
 function criarTarefa() {
     var tarefaOriginal = document.getElementById('tasks-box');
     var novaTarefa = tarefaOriginal.cloneNode(true);
-    var local= document.getElementById('divisao');
-    
+    var local = document.getElementById('divisao');
+
     local.appendChild(novaTarefa);
 }
 
 
-  
 
