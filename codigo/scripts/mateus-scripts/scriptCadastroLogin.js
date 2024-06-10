dd2cddd0eacf138ceea4fccc95e033e176749ab4
botellhx <maat9779@gmail.com>
2024-06-10 19:19:25 -0300

    Login&Cadastro

---
+++ codigo/scripts/mateus-scripts/scriptCadastroLogin.js
@@ -0,0 +1,22 @@
+document.getElementById('signup-form').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const response = await fetch('http://localhost:3000/usuarios', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, email, password })
+    });
+
+    if (response.ok) {
+        alert('Usuário cadastrado com sucesso!');
+        document.getElementById('signup-form').reset();
+    } else {
+        alert('Erro ao cadastrar usuário.');
+    }
+});
